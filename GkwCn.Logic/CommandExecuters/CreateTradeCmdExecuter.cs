a350121de6a4c87fd4cc22df9920692e14ed1134
gk-project <306655408@qq.com>
2013-06-23 04:20:32 +0800

    add trade createtradecmd executer

---
+++ GkwCn.Logic/CommandExecuters/CreateTradeCmdExecuter.cs
@@ -0,0 +1,20 @@
+﻿using GkwCn.Domains.Business;
+using GkwCn.Framework.Commands;
+using GkwCn.Models.Commands.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GkwCn.Logic.CommandExecuters
+{
+    public class CreateTradeCmdExecuter : AbstractCommandExecuter<CreateTradeCmd>
+    {
+        public override object Execute(CreateTradeCmd cmd)
+        {
+            var domain = new Trade();
+            return domain;
+        }
+    }
+}
